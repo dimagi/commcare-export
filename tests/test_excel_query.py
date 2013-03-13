0f3e13938acc899d15f380105cd3068e169d2821
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-13 15:14:46 -0400

    Add failing test for creating filters

--- tests/test_excel_query.py
+++ tests/test_excel_query.py
@@ -1,4 +1,5 @@
 import unittest
+import pprint
 import os.path
 
 import openpyxl
@@ -16,9 +17,26 @@ class TestExcelQuery(unittest.TestCase):
 
         test_cases = [
             ('001_JustDataSource.xlsx', Apply(Reference("api_data"), Literal("form"))),
+            
+            ('002_DataSourceAndFilters.xlsx', 
+             Apply(Reference("api_data"), 
+                   Literal("form"), 
+                   Literal({
+                       'filter': {
+                           'and': [
+                               {'term': { 'app_id': 'foobizzle' }},
+                               {'term': { 'type': 'intake' }}
+                            ]
+                        }
+                   }))),
         ]
 
         for filename, minilinq in test_cases:
             abs_path = os.path.join(os.path.dirname(__file__), filename)
             compiled = compile_sheet(openpyxl.load_workbook(abs_path).get_active_sheet()) 
+            # Print will be suppressed by pytest unless it fails
+            if not (compiled == minilinq):
+                pprint.pprint(compiled.to_jvalue())
+                print '!='
+                pprint.pprint(minilinq.to_jvalue())
             assert compiled == minilinq
