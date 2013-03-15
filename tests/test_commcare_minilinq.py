c4a0ef24f6df02037155b98319140ec0e532de54
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-15 17:18:54 -0400

    Add test that mapping over the api results works for deep referencing

--- tests/test_commcare_minilinq.py
+++ tests/test_commcare_minilinq.py
@@ -16,12 +16,31 @@ class TestCommCareMiniLinq(unittest.TestCase):
     def test_eval(self):
         client = MockCommCareHqClient({
             'form': {
-                urllib.urlencode({'_search': simplejson.dumps({"filter":"test1"})}): [1, 2, 3]
+                urllib.urlencode({'_search': simplejson.dumps({"filter":"test1"})}): [1, 2, 3],
+                urllib.urlencode({'_search': simplejson.dumps({"filter":"test2"})}): [
+                    { 'x': [{ 'y': 1 }, {'y': 2}] },
+                    { 'x': [{ 'y': 3 }, {'z': 4}] },
+                    { 'x': [{ 'y': 5 }] }
+                ],
             }
         })
 
         env = BuiltInEnv() | CommCareHqEnv(client) | JsonPathEnv({}) # {'form': api_client.iterate('form')})
 
-        assert Apply(Reference('api_data'),
-                     Literal('form'),
-                     Literal({"filter": 'test1'})).eval(env) == [1, 2, 3]
+        assert list(Apply(Reference('api_data'),
+                          Literal('form'),
+                          Literal({"filter": 'test1'})).eval(env)) == [1, 2, 3]
+
+        # just check that we can still apply some deeper xpath by mapping; first ensure the basics work
+        assert list(Apply(Reference('api_data'),
+                                     Literal('form'),
+                                     Literal({"filter": 'test2'})).eval(env)) == [
+                    { 'x': [{ 'y': 1 }, {'y': 2}] },
+                    { 'x': [{ 'y': 3 }, {'z': 4}] },
+                    { 'x': [{ 'y': 5 }] }
+                ]
+
+        assert list(FlatMap(source=Apply(Reference('api_data'),
+                                     Literal('form'),
+                                     Literal({"filter": 'test2'})),
+                        body=Reference('x[*].y')).eval(env)) == [1, 2, 3, 5]
