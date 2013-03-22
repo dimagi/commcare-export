fd69dad257c403bdccf07d1a64ba7dc8044fa41c
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-22 14:49:52 -0400

    Get ready for Cheeseshop distribution

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
+import os.path
+import sys
+import subprocess
 import setuptools 
+
+# Build README.txt from README.md if not present, and if we are actually building for distribution to pypi
+if not os.path.exists('README.txt') and 'sdist' in sys.argv:
+    subprocess.call(['pandoc', '--to=rst', '--smart', '--output=README.txt', 'README.md'])
+
+readme = 'README.txt' if os.path.exists('README.txt') else 'README.md'
 
 setuptools.setup(   
     name = "commcare-export",
     version = "0.1",
+    description = 'A command-line tool (and Python library) to extract data from CommCareHQ into a SQL database or Excel workbook',
+    long_description = open(readme).read(),
+    author = 'Dimagi',
+    author_email = 'information@dimagi.com',
     url = "https://github.com/dimagi/commcare-export",
-    maintainer = "CommCareHQ Team",
-    maintainer_email = "information@dimagi.com",
     entry_points = { 'console_scripts': ['commcare-export = commcare_export.cli:entry_point'] },
-    packages = setuptools.find_packages(),
+    packages = ['commcare_export'],
+    license = 'MIT',
     install_requires = ['jsonpath_rw>=0.7', 
                         'requests',
                         'simplejson',
