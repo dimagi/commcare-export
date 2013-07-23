0feabfcc935d4b0708208eee208faddebaf94a0a
Kenn Knowles <kknowles@dimagi.com>
2013-07-23 19:06:28 -0400

    Use the __version__ in User-Agent and distribution

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
 import os.path
 import sys
 import glob
+import re
 import io
 import subprocess
 import setuptools 
 from setuptools.command.test import test as TestCommand
 
+VERSION_PATH='commcare_export/VERSION'
+
 # Build README.txt from README.md if not present, and if we are actually building for distribution to pypi
 if not os.path.exists('README.txt') and 'sdist' in sys.argv:
     subprocess.call(['pandoc', '--to=rst', '--output=README.txt', 'README.md'])
+
+# Overwrite VERSION if we are actually building for a distribution to pypi
+# This code path requires dependencies, etc, to be available
+if 'sdist' in sys.argv:
+    import commcare_export.version
+    with io.open(VERSION_PATH, 'w', encoding='ascii') as fh:
+        fh.write(commcare_export.version.git_version())
+
+# This import requires either commcare_export/VERSION or to be in a git clone (as does the package in general)
+import commcare_export
+version = commcare_export.__version__
+
+# Crash if the VERSION is not a simple version and it is going to register or upload
+if 'register' in sys.argv or 'upload' in sys.argv:
+    if not re.match('\d+\.\d+\.\d+', version):
+        print 'Version %s is not an appropriate version for publicizing!' % version
+        sys.exit(1)
 
 readme = 'README.txt' if os.path.exists('README.txt') else 'README.md'
 
@@ -26,7 +46,7 @@ class PyTest(TestCommand):
 
 setuptools.setup(   
     name = "commcare-export",
-    version = "0.11.2",
+    version = version,
     description = 'A command-line tool (and Python library) to extract data from CommCareHQ into a SQL database or Excel workbook',
     long_description = io.open(readme, encoding='utf-8').read(),
     author = 'Dimagi',
@@ -35,6 +55,7 @@ setuptools.setup(
     entry_points = { 'console_scripts': ['commcare-export = commcare_export.cli:entry_point'] },
     packages = ['commcare_export'],
     data_files = [(os.path.join('share', 'commcare-export', 'examples'), glob.glob('examples/*.json') + glob.glob('examples/*.xlsx'))],
+    package_data = {'': ['VERSION']},
     license = 'MIT',
     install_requires = [
         'alembic',
