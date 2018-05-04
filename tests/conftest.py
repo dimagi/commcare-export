b8edf34451ad9d580696dbee2b04f7bd13f798eb
Simon Kelly <skelly@dimagi.com>
2018-05-04 13:48:06 +0200

    add mssql to db list

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, absolute_import, division, generators, nested_scopes
 
+import logging
 import uuid
 
 import pytest
 import sqlalchemy
+from sqlalchemy.exc import DBAPIError
 
 TEST_DB = 'test_commcare_export_%s' % uuid.uuid4().hex
+
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler())
 
 @pytest.fixture(scope="class", params=[
     {
@@ -16,36 +21,36 @@ TEST_DB = 'test_commcare_export_%s' % uuid.uuid4().hex
     {
         'url': 'mysql+pymysql://travis@/%s?charset=utf8',
     },
-], ids=['postgres', 'mysql'])
+    {
+        'url': 'mssql+pyodbc://SA:Password@123@localhost/%s?driver=ODBC+Driver+17+for+SQL+Server',
+        'admin_db': 'master'
+    }
+], ids=['postgres', 'mysql', 'mssql'])
 def db_params(request):
-    try:
-        sudo_engine = sqlalchemy.create_engine(request.param['url'] % request.param.get('admin_db', ''), poolclass=sqlalchemy.pool.NullPool)
-    except TypeError:
-        pass
-
-    try:
-        db_connection_url = request.param['url'] % TEST_DB
-    except TypeError:
-        db_connection_url = request.param['url']
+    db_url = request.param['url']
+    sudo_engine = sqlalchemy.create_engine(db_url % request.param.get('admin_db', ''), poolclass=sqlalchemy.pool.NullPool)
+    db_connection_url = db_url % TEST_DB
 
     def tear_down():
         with sudo_engine.connect() as conn:
             conn.execute('rollback')
+            if 'mssql' in db_url:
+                conn.connection.connection.autocommit = True
             conn.execute('drop database if exists %s' % TEST_DB)
 
     try:
         with sqlalchemy.create_engine(db_connection_url).connect():
             pass
-    except (sqlalchemy.exc.OperationalError, sqlalchemy.exc.InternalError):
+    except (sqlalchemy.exc.OperationalError, sqlalchemy.exc.InternalError, DBAPIError):
         with sudo_engine.connect() as conn:
             conn.execute('rollback')
+            if 'mssql' in db_url:
+                conn.connection.connection.autocommit = True
             conn.execute('create database %s' % TEST_DB)
     else:
-        if not request.param.get('skip_create_teardown', False):
-            raise Exception('Database %s already exists; refusing to overwrite' % TEST_DB)
+        raise Exception('Database %s already exists; refusing to overwrite' % TEST_DB)
 
-    if not request.param.get('skip_create_teardown', False):
-        request.addfinalizer(tear_down)
+    request.addfinalizer(tear_down)
 
     params = request.param.copy()
     params['url'] = db_connection_url
