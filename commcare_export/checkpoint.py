ff8f9583ef1bcdb596a9f7751d784efa9665d4af
Simon Kelly <skelly@dimagi.com>
2018-08-14 10:33:41 +0200

    use sqlalchemy orm

--- commcare_export/checkpoint.py
+++ commcare_export/checkpoint.py
@@ -3,13 +3,44 @@ import logging
 import uuid
 
 import os
+from contextlib import contextmanager
 
 import dateutil.parser
+from sqlalchemy import Column, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 from commcare_export.writers import SqlMixin
 
 logger = logging.getLogger(__name__)
 repo_root = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+
+Base = declarative_base()
+
+
+class ExportRun(Base):
+    __tablename__ = 'commcare_export_runs'
+
+    id = Column(String, primary_key=True)
+    query_file_name = Column(String)
+    query_file_md5 = Column(String)
+    since_param = Column(String)
+    time_of_run = Column(String)
+    final = Column(Boolean)
+
+
+@contextmanager
+def session_scope(Session):
+    """Provide a transactional scope around a series of operations."""
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 
 class CheckpointManager(SqlMixin):
@@ -20,6 +51,7 @@ class CheckpointManager(SqlMixin):
         super(CheckpointManager, self).__init__(db_url, poolclass=poolclass)
         self.query = query
         self.query_md5 = query_md5
+        self.Session = sessionmaker(self.engine)
 
     def set_batch_checkpoint(self, checkpoint_time):
         self._set_checkpoint(checkpoint_time, False)
@@ -33,14 +65,15 @@ class CheckpointManager(SqlMixin):
     def _set_checkpoint(self, checkpoint_time, final):
         logger.info('Setting %s checkpoint: %s', 'final' if final else 'batch', checkpoint_time)
         checkpoint_time = checkpoint_time or datetime.datetime.utcnow()
-        self._insert_checkpoint(
-            id=uuid.uuid4().hex,
-            query_file_name=self.query,
-            query_file_md5=self.query_md5,
-            since_param=checkpoint_time.isoformat(),
-            time_of_run=datetime.datetime.utcnow().isoformat(),
-            final=final
-        )
+        with session_scope(self.Session) as session:
+            session.add(ExportRun(
+                id=uuid.uuid4().hex,
+                query_file_name=self.query,
+                query_file_md5=self.query_md5,
+                since_param=checkpoint_time.isoformat(),
+                time_of_run=datetime.datetime.utcnow().isoformat(),
+                final=final
+            ))
 
     def create_checkpoint_table(self):
         from alembic import command, config
@@ -50,32 +83,17 @@ class CheckpointManager(SqlMixin):
             cfg.attributes['connection'] = connection
             command.upgrade(cfg, "head")
 
-    def _insert_checkpoint(self, **row):
-        table = self.table(self.table_name)
-        insert = table.insert().values(**row)
-        self.connection.execute(insert)
-
     def _cleanup(self):
-        sql = """
-           DELETE FROM {}
-           WHERE final = :final
-           AND query_file_md5 = :md5
-        """.format(self.table_name)
-        self.connection.execute(
-            self.sqlalchemy.sql.text(sql),
-            final=False,
-            md5=self.query_md5,
-        )
+        with session_scope(self.Session) as session:
+            session.query(ExportRun).filter_by(
+                final=False, query_file_md5=self.query_md5
+            ).delete()
 
     def get_time_of_last_run(self):
         if 'commcare_export_runs' in self.metadata.tables:
-            sql = """
-                SELECT since_param FROM commcare_export_runs
-                WHERE query_file_md5 = :query_file_md5 ORDER BY since_param DESC
-            """
-            cursor = self.connection.execute(
-                self.sqlalchemy.sql.text(sql),
-                query_file_md5=self.query_md5
-            )
-            for row in cursor:
-                return row[0]
+            with session_scope(self.Session) as session:
+                run = session.query(ExportRun).filter_by(
+                    query_file_md5=self.query_md5
+                ).order_by(ExportRun.since_param.desc()).first()
+                if run:
+                    return run.since_param
