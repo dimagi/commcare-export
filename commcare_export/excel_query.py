e831d141f7cff5f5af0e860fa9b7bdb6bf39f429
Cory Zue <czue@dimagi.com>
2020-05-27 14:19:13 +0200

    fix tests if no olumn found

--- commcare_export/excel_query.py
+++ commcare_export/excel_query.py
@@ -273,7 +273,7 @@ def parse_sheet(worksheet, mappings=None):
     else:
         output_table_name = worksheet.title
     output_headings = get_column_by_name(worksheet, 'field')
-    output_types = get_column_by_name(worksheet, 'data type')
+    output_types = get_column_by_name(worksheet, 'data type') or []
     output_fields = compile_fields(worksheet, mappings=mappings)
 
     if not output_fields:
