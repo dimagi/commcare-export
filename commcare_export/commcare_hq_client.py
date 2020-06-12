136cd069bfccbd7282a56d4133b0ba1d87f43973
Charlie Garrett <garrett@concurix.com>
2020-06-12 13:59:20 -0700

    Fix Python 2 tests by making new exception's string representation explicit.

--- commcare_export/commcare_hq_client.py
+++ commcare_export/commcare_hq_client.py
@@ -53,6 +53,9 @@ class ResourceRepeatException(Exception):
     def __init__(self, message):
         self.message = message
 
+    def __str__(self):
+        return self.message
+
 
 class CommCareHqClient(object):
     """
