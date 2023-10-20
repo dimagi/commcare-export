a814d71afae1e4d79e66de895599e4cf0039cf4b
Chris Smit <chris.smit@dimagi.com>
2023-10-20 09:44:42 +0200

    Increase default page size for UCRs and change default value setting

--- commcare_export/commcare_minilinq.py
+++ commcare_export/commcare_minilinq.py
@@ -102,7 +102,7 @@ DATE_PARAMS = {
 
 def get_paginator(
     resource,
-    page_size=1000,
+    page_size=None,
     pagination_mode=PaginationMode.date_indexed,
 ):
     return {
@@ -134,7 +134,7 @@ class CommCareHqEnv(DictEnv):
     CommCareHq API.
     """
 
-    def __init__(self, commcare_hq_client, until=None, page_size=1000):
+    def __init__(self, commcare_hq_client, page_size=None, until=None):
         self.commcare_hq_client = commcare_hq_client
         self.until = until
         self.page_size = page_size
@@ -177,7 +177,8 @@ class SimplePaginator(object):
     Paginate based on the 'next' URL provided in the API response.
     """
 
-    def __init__(self, page_size=1000, params=None):
+    def __init__(self, page_size=None, params=None):
+        page_size = page_size if page_size else 1000
         self.page_size = page_size
         self.params = params
 
@@ -224,7 +225,8 @@ class DatePaginator(SimplePaginator):
 
     DEFAULT_PARAMS = object()
 
-    def __init__(self, since_field, page_size=1000, params=DEFAULT_PARAMS):
+    def __init__(self, since_field, page_size=None, params=DEFAULT_PARAMS):
+        page_size = page_size if page_size else 1000
         params = DATE_PARAMS[
             since_field] if params is DatePaginator.DEFAULT_PARAMS else params
         super(DatePaginator, self).__init__(page_size, params)
@@ -268,6 +270,9 @@ class DatePaginator(SimplePaginator):
 
 
 class UCRPaginator(SimplePaginator):
+    def __init__(self, page_size=None, *args, **kwargs):
+        page_size = page_size if page_size else 10000
+        super().__init__(page_size, *args, **kwargs)
 
     def next_page_params_from_batch(self, batch):
         params = super(UCRPaginator, self).next_page_params_from_batch(batch)
