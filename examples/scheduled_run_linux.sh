551eaee2ff4986e925da4917925e3a8bd0b00c0f
Charl Smit <csmit@dimagi.com>
2024-04-04 16:02:38 +0200

    Create sample Linux script

---
+++ examples/scheduled_run_linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+commcare-export --output-format <output_format> \
+ --output <db_connection_string> \
+ --query <path_to_query_file> \
+ --project <domain> \
+ --commcare-hq https://commcarehq.org \
+ --auth-mode apikey \
+ --password <password> \
